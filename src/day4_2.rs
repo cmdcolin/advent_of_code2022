771b5e5792d2266be951fc1e547eb522a0157f96
Colin <colin.diesh@gmail.com>
2022-12-04 17:13:18 -0700

    Add day4

---
+++ src/day4_2.rs
@@ -0,0 +1,20 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut score = 0;
+    for line in stdin.lock().lines() {
+        let l = line.unwrap();
+        let v = l.split(',').collect::<Vec<_>>();
+        let r1 = v[0].split('-').collect::<Vec<_>>();
+        let r2 = v[1].split('-').collect::<Vec<_>>();
+        let r1s = r1[0].parse::<i32>().unwrap();
+        let r1e = r1[1].parse::<i32>().unwrap();
+        let r2s = r2[0].parse::<i32>().unwrap();
+        let r2e = r2[1].parse::<i32>().unwrap();
+        if r1e >= r2s && r2e >= r1s {
+            score += 1;
+        }
+    }
+    println!("{}", score);
+}
